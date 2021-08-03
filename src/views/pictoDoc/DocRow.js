c18d879e768b1e1a2380f7965803a078dacdd74d
sander hagenaars <sander.ha@hotmail.com>
2021-08-03 13:08:04 +0200

    row effects

--- src/views/pictoDoc/DocRow.js
+++ src/views/pictoDoc/DocRow.js
@@ -9,6 +9,8 @@ import useRevealOnHover from '../../hooks/useRevealOnHover';
 export default function DocRow({ deleteMethod, deleteCardMethod, cards, row }) {
     const { styleObj, handleHover, handleHoverLeave } = useRevealOnHover();
 
+    const hasCards = () => row.cardsIds.length;
+
     const renderCards = () => {
         return row.cardsIds.map((cardId, i) => {
             const card = cards.find(card => card.id === cardId);
@@ -37,18 +39,19 @@ export default function DocRow({ deleteMethod, deleteCardMethod, cards, row }) {
             <Col>
 
                 <Droppable droppableId={`row-${row.id}`} direction="horizontal">
-                    {(provided) => {
+                    {(provided, snapshot) => {
                         return (
                             <div
                                 ref={provided.innerRef}
                                 {...provided.droppableProps}
-                                className="doc-row card mb-2"
+                                className={`doc-row card mb-2${snapshot.isDraggingOver ? " doc-row--is-dragged-over" : ""}${!hasCards() ? " doc-row--is-empty" : ""}`}
                                 onMouseLeave={handleHoverLeave}
                                 onMouseEnter={handleHover}
                             >
-                                <Row className="no-gutters flex-nowrap">
+                                <Row className={`no-gutters flex-nowrap`}>
                                     {renderCards()}
                                     {provided.placeholder}
+                                    {!hasCards() && <div className="doc-row__empty-text">Drag pictures here!</div>}
                                 </Row>
                                 <Row>
                                     <Col className="text-right">
