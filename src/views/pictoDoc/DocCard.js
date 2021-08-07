b3ea5fca356d7e6dad09a523701801713e95450e
sander hagenaars <sander.ha@hotmail.com>
2021-08-07 14:08:48 +0200

    fix card alt

--- src/views/pictoDoc/DocCard.js
+++ src/views/pictoDoc/DocCard.js
@@ -3,7 +3,7 @@ import CardImg from 'react-bootstrap/CardImg'
 
 export default function DocCard({ card, deleteMethod }) {
     return <Card body className="doc-card m-2 text-center">
-        <CardImg width="100%" src={card.img} alt={card.img} />
+        <CardImg width="100%" src={card.img} alt={card.name} />
         <div className="doc-card__delete">
             <button className="btn btn-sm btn-danger mt-2 " onClick={() => deleteMethod(card)}>
                 Delete
