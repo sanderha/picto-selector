ba9b78b9263a53327a7b9824f3d07a2645c9e737
sander hagenaars <sander.ha@hotmail.com>
2021-08-08 19:11:00 +0200

    styling

--- src/views/pictoDoc/ReadOnlyDocCard.js
+++ src/views/pictoDoc/ReadOnlyDocCard.js
@@ -3,7 +3,7 @@ import CardImg from 'react-bootstrap/CardImg'
 
 export default function ReadOnlyDocCard({ card, isFloating }) {
 
-    return <Card body className={`bg-primary doc-card doc-card--read-only doc-card--small text-center ${isFloating ? "doc-card--floating" : ""}`}>
+    return <Card body className={`doc-card doc-card--read-only doc-card--small text-center ${isFloating ? "doc-card--floating" : ""}`}>
         <CardImg width="100%" src={card.img} alt={card.name} />
     </Card>
 }
