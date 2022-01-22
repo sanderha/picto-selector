b9d9c8eb8e9a9cbad69a49031e6b11524d97ef6c
sander hagenaars <sander.ha@hotmail.com>
2022-01-23 00:00:56 +0100

    footer tekst

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -1,5 +1,7 @@
 export default function Footer() {
     return <footer className="footer text-center">
         For a more advanced version with many more features, check out the desktop application at <a rel="noreferrer" href="https://www.pictoselector.eu/" target="_blank">https://www.pictoselector.eu/</a>
+        <br/>
+        Ideas or suggestions? Send them to <a href="mailto:sander.ha@hotmail.com">sander.ha@hotmail.com</a>
     </footer>
 }
